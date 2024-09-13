f299fd0272a657e046c1fce95553e01743713348
ParkMinhee02 <sundu32672@naver.com>
2024-09-13 19:40:57 +0900

    first commit

---
+++ src/data.js
@@ -0,0 +1,25 @@
+let data = [
+	{
+		id: 0,
+		title: '내가 좋아하는 침구',
+		content: '편안한 잠을 위한 푹신푹신한 침구',
+		price: 25000,
+		images: '/img/product_01.jpg'
+	},
+	{
+		id: 1,
+		title: '내가 좋아하는 모빌',
+		content: '나를 즐겁게 해주는 예쁜 모빌',
+		price: 56000,
+		images: '/img/product_02.jpg'
+	},
+	{
+		id: 2,
+		title: '내가 좋아하는 인테리어',
+		content: '인테리어 소품으로 좋아요.',
+		price: 13000,
+		images: '/img/product_03.jpg'
+	}
+]
+
+export default data;
